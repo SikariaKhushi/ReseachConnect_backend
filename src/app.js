b77ef48bed990a930a9fd6dc94a4090eb07a77d3
SikariaKhushi <khushi.sikaria2022@vitstudent.ac.in>
2025-05-04 15:53:24 +0530

    fix: cors error

--- src/app.js
+++ src/app.js
@@ -27,12 +27,12 @@ connectMongoDB();
 
 const app = express();
 app.set('trust proxy', 1);
+app.use(cors({ origin: '*' }));
 
 app.use(helmet());
 app.use(compression());
 app.use(limiter)
 app.use(express.json());
-app.use(cors('*'));
 app.use(express.urlencoded({ extended: true }));
 
 //metrics route
